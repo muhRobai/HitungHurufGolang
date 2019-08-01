88dc3122de0cd94f09c53f162e3b5fa21342cd8c
kesetbasah <robaimuh@gmail.com>
2019-08-01 20:45:29 +0700

    sum huruf

---
+++ huruf/Huruf.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+	"Nomer_Nol/sum"
+	"fmt"
+)
+
+func main(){
+	sum.Sum("omama")
+	fmt.Println("")
+	fmt.Println("===================||==================||============")
+	sum.Sum("oasam")
+	fmt.Println("")
+}
